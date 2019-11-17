b23d8d77b5008b427bdc42cf9106ecf8f2317f8a
FeDLviv <fed.lviv@gmail.com>
2019-11-17 15:27:30 +0200

    Update security (add whitelist IP to the endpoint -> /instances)

--- src/main/java/net/omisoft/admin/configuration/SecurityConfiguration.java
+++ src/main/java/net/omisoft/admin/configuration/SecurityConfiguration.java
@@ -1,13 +1,24 @@
 package net.omisoft.admin.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final String hasIpRangeAccessExpresion;
+
+    public SecurityConfiguration(@Value("${app.ip.whitelist}") List<String> ipWhitelist) {
+        hasIpRangeAccessExpresion = ipWhitelist.stream()
+                .collect(Collectors.joining("') or hasIpAddress('", "hasIpAddress('", "')"));
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -18,7 +29,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers("/assets/**").permitAll()
                 //TODO delete line -> "If you protect the /instances endpoint don’t forget to configure the username and password on your SBA-Client using spring.boot.admin.client.username and spring.boot.admin.client.password."
-                .antMatchers("/instances/**").permitAll()
+                .antMatchers("/instances/**").access("isAuthenticated() or " + hasIpRangeAccessExpresion)
                 .antMatchers("/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
